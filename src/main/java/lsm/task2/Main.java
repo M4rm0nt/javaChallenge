fcc001f2f8332903824c75d15735fb5ce9c9d655
marmont <mar.mont@example.com>
2025-04-05 14:45:06 +0200

    update

--- src/main/java/lsm/task2/Main.java
+++ src/main/java/lsm/task2/Main.java
@@ -12,31 +12,52 @@ public class Main {
         List<Integer> c2 = Arrays.asList(2,4,6,7);
 
         Set<Integer> resultD = difference(c1,c2);
+        //  Set<Integer> resultD = difference(c2,c1);
         System.out.println(resultD);
 
-        Set<Integer> resultDNOJDK = differenceNOJDK(c1,c2);
-        System.out.println(resultDNOJDK);
-
+        // JDK implementierung
         Set<Integer> resultU = union(c1,c2);
         System.out.println(resultU);
-
-        Set<Integer> resultUNOJDK = unionNOJDK(c1,c2);
-        System.out.println(resultUNOJDK);
 
         Set<Integer> resultI = intersection(c1,c2);
         System.out.println(resultI);
 
+        // Selbst implementiert
+        Set<Integer> resultDNOJDK = differenceNOJDK(c1,c2);
+        //  Set<Integer> resultDNOJDK = differenceNOJDK(c2,c1);
+        System.out.println(resultDNOJDK);
+
+        Set<Integer> resultUNOJDK = unionNOJDK(c1,c2);
+        System.out.println(resultUNOJDK);
+
         Set<Integer> resultINOJDK = intersectionNOJDK(c1,c2);
         System.out.println(resultINOJDK);
-
     }
 
+    // JDK implementierung von Differenzmengen
     static <T> Set<T> difference(final Collection<T> c1, final Collection<T> c2) {
         final Set<T> results = new HashSet<>(c1);
         results.removeAll(c2);
         return  results;
     }
 
+    // JDK implementierung von Vereinigungsmenge
+    static <T> Set<T> union(final Collection<T> c1, final Collection<T> c2) {
+        final Set<T> result = new HashSet<>(c1);
+        result.addAll(c2);
+        return  result;
+    }
+
+    // JDK implementierung von Schnittmenge
+    static <T> Set<T> intersection(final Collection<T> c1, final Collection<T> c2) {
+        final Set<T> results = new HashSet<>(c1);
+        results.retainAll(c2);
+        return  results;
+    }
+
+    //-------------------------------------------------
+    //-------   Differenzmenge von zwei Mengen
+    //-------------------------------------------------
     static <T> Set<T> differenceNOJDK(final Collection<T> c1, final Collection<T> c2) {
         final Map<T, Long> result = new HashMap<>();
         populateFromCollection1(c1, result);
@@ -44,12 +65,9 @@ public class Main {
         return removeDuplicatesInCollection(result);
     }
 
-    static <T> Set<T> union(final Collection<T> c1, final Collection<T> c2) {
-        final Set<T> result = new HashSet<>(c1);
-        result.addAll(c2);
-        return  result;
-    }
-
+    //-------------------------------------------------
+    //-------   Vereinigungsmenge von zwei Mengen
+    //-------------------------------------------------
     static <T> Set<T> unionNOJDK(final Collection<T> c1, final Collection<T> c2) {
         final Set<T> result = new HashSet<>();
 
@@ -64,12 +82,9 @@ public class Main {
         return result;
     }
 
-    static <T> Set<T> intersection(final Collection<T> c1, final Collection<T> c2) {
-        final Set<T> results = new HashSet<>(c1);
-        results.retainAll(c2);
-        return  results;
-    }
-
+    //-------------------------------------------------
+    //-------   Schnittmenge von zwei Mengen
+    //-------------------------------------------------
     static <T> Set<T> intersectionNOJDK(final Collection<T> c1, final Collection<T> c2) {
         final Map<T, Long> results = new HashMap<>();
         populateFromCollection1(c1, results);
